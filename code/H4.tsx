427c95142d81a65dda76333c5f3cec4cbe552b21
Fernando Hurtado <fernandohur@gmail.com>
2019-08-22 16:47:31 +0200

    Adde H1..H4

--- code/H4.tsx
+++ code/H4.tsx
@@ -1,61 +1,19 @@
+import * as System from "baseui/typography";
+import { addPropertyControls, ControlType } from "framer";
 import * as React from "react";
-import * as System from "baseui";
-import { ControlType, PropertyControls, addPropertyControls } from "framer";
-import { controls, merge } from "./generated/H4";
 import { withHOC } from "./withHOC";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
-
-const InnerH4: React.SFC = props => {
-  return <System.H4 {...props} style={style} />;
+const InnerH4: React.SFC<any> = ({ text, ["children"]: _, ...props }) => {
+  return <System.H4 {...props}>{text}</System.H4>;
 };
 
 export const H4 = withHOC(InnerH4);
 
 H4.defaultProps = {
-  width: 150,
-  height: 50
+  width: 360,
+  height: 200
 };
 
 addPropertyControls(H4, {
-  children: merge(controls.children, {}),
-  src: merge(controls.src, {}),
-  alignContent: merge(controls.alignContent, {}),
-  alignItems: merge(controls.alignItems, {}),
-  alignSelf: merge(controls.alignSelf, {}),
-  flexDirection: merge(controls.flexDirection, {}),
-  display: merge(controls.display, {}),
-  flex: merge(controls.flex, {}),
-  gridAutoFlow: merge(controls.gridAutoFlow, {}),
-  gridColumnGap: merge(controls.gridColumnGap, {}),
-  gridGap: merge(controls.gridGap, {}),
-  gridRowGap: merge(controls.gridRowGap, {}),
-  justifyContent: merge(controls.justifyContent, {}),
-  justifyItems: merge(controls.justifyItems, {}),
-  justifySelf: merge(controls.justifySelf, {}),
-  position: merge(controls.position, {}),
-  width: merge(controls.width, {}),
-  minWidth: merge(controls.minWidth, {}),
-  maxWidth: merge(controls.maxWidth, {}),
-  height: merge(controls.height, {}),
-  minHeight: merge(controls.minHeight, {}),
-  maxHeight: merge(controls.maxHeight, {}),
-  overflow: merge(controls.overflow, {}),
-  margin: merge(controls.margin, {}),
-  marginTop: merge(controls.marginTop, {}),
-  marginRight: merge(controls.marginRight, {}),
-  marginBottom: merge(controls.marginBottom, {}),
-  marginLeft: merge(controls.marginLeft, {}),
-  padding: merge(controls.padding, {}),
-  paddingTop: merge(controls.paddingTop, {}),
-  paddingRight: merge(controls.paddingRight, {}),
-  paddingBottom: merge(controls.paddingBottom, {}),
-  paddingLeft: merge(controls.paddingLeft, {}),
-  left: merge(controls.left, {}),
-  top: merge(controls.top, {}),
-  right: merge(controls.right, {}),
-  bottom: merge(controls.bottom, {})
+  text: { type: ControlType.String, defaultValue: "HEADING 4" }
 });
