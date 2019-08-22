76cccd02b2bcb6d368b5c87481cb3eb21fd28c31
Fernando Hurtado <fernandohur@gmail.com>
2019-08-22 16:29:26 +0200

    Added PhoneInput

--- code/PhoneInput.tsx
+++ code/PhoneInput.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
-import * as System from "baseui";
+import * as System from "baseui/phone-input";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { controls, merge } from "./generated/PhoneInput";
 import { withHOC } from "./withHOC";
 
-const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
-
-const InnerPhoneInput: React.SFC = props => {
-  return <System.PhoneInput {...props} style={style} />;
+const InnerPhoneInput: React.SFC<any> = ({ text, ...props }) => {
+  const [currentText, setText] = React.useState(text);
+  const [country, setCountry] = React.useState(System.COUNTRIES.US);
+  return (
+    <System.PhoneInput
+      text={currentText}
+      country={country}
+      onTextChange={event => {
+        setText(event.currentTarget.value);
+      }}
+      onCountryChange={(event: any) => {
+        setCountry(event.option);
+      }}
+    />
+  );
 };
 
 export const PhoneInput = withHOC(InnerPhoneInput);
@@ -21,8 +29,6 @@ PhoneInput.defaultProps = {
 };
 
 addPropertyControls(PhoneInput, {
-  text: merge(controls.text, {}),
-  size: merge(controls.size, {}),
-  maxDropdownHeight: merge(controls.maxDropdownHeight, {}),
-  maxDropdownWidth: merge(controls.maxDropdownWidth, {})
+  text: merge(controls.text, { defaultValue: "650413182" }),
+  size: merge(controls.size, {})
 });
