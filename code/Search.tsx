2c5e0d481800d6ace774fcd2e362b4cfdfe86cdb
Fernando Hurtado <fernandohur@gmail.com>
2019-08-22 16:01:15 +0200

    Added Search and StarRating

--- code/Search.tsx
+++ code/Search.tsx
@@ -1,7 +1,6 @@
+import BaseUiSearch from "baseui/icon/search";
+import { addPropertyControls, ControlType } from "framer";
 import * as React from "react";
-import * as System from "baseui";
-import { ControlType, PropertyControls, addPropertyControls } from "framer";
-import { controls, merge } from "./generated/Search";
 import { withHOC } from "./withHOC";
 
 const style: React.CSSProperties = {
@@ -10,19 +9,16 @@ const style: React.CSSProperties = {
 };
 
 const InnerSearch: React.SFC = props => {
-  return <System.Search {...props} style={style} />;
+  return <BaseUiSearch {...props} style={style} />;
 };
 
 export const Search = withHOC(InnerSearch);
 
 Search.defaultProps = {
-  width: 150,
+  width: 50,
   height: 50
 };
 
 addPropertyControls(Search, {
-  children: merge(controls.children, {}),
-  size: merge(controls.size, {}),
-  color: merge(controls.color, {}),
-  title: merge(controls.title, {})
+  color: { type: ControlType.Color, defaultValue: "#121212" }
 });
--- code/StarRating.tsx
+++ code/StarRating.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as System from "baseui";
+import * as System from "baseui/rating";
 import { ControlType, PropertyControls, addPropertyControls } from "framer";
 import { controls, merge } from "./generated/StarRating";
 import { withHOC } from "./withHOC";
@@ -9,18 +9,29 @@ const style: React.CSSProperties = {
   height: "100%"
 };
 
-const InnerStarRating: React.SFC = props => {
-  return <System.StarRating {...props} style={style} />;
+const InnerStarRating: React.SFC = ({ value, ...props }) => {
+  const [currentValue, setValue] = React.useState(value);
+  return (
+    <System.StarRating
+      value={currentValue}
+      onChange={({ value }) => setValue(value)}
+      {...props}
+    />
+  );
 };
 
 export const StarRating = withHOC(InnerStarRating);
 
 StarRating.defaultProps = {
   width: 150,
-  height: 50
+  height: 25
 };
 
 addPropertyControls(StarRating, {
-  value: merge(controls.value, {}),
-  numItems: merge(controls.numItems, {})
+  value: merge(controls.value, { min: 0, max: 100, defaultValue: 2 }),
+  numItems: merge(controls.numItems, {
+    min: 0,
+    max: 100,
+    defaultValue: 5
+  })
 });
