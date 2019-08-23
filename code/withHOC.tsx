1523a378b6a3f1846ba8e846ded5a2ad14949d48
iKettles <iain@framer.com>
2019-08-23 13:19:43 +0200

    Implement ThemeSwitcher

--- code/withHOC.tsx
+++ code/withHOC.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import { ThemeProvider } from "./utils/ThemeProvider";
+import { addListener } from "./utils/ThemeManagement";
 
 export function withHOC(Component): React.SFC {
   return (props: any) => {
+    const [currentTheme, setCurrentTheme] = React.useState()
+
+    React.useEffect(() => {
+      const unsubscribe = addListener(theme => {
+          setCurrentTheme(theme)
+      })
+      return unsubscribe;
+    }, []);
+
     return (
-      <ThemeProvider theme={props.theme}>
+      <ThemeProvider theme={currentTheme}>
         <Component {...props} />
       </ThemeProvider>
     )
