6ec1413905ecb338e4d86db78ffd9a201ad78d11
Fernando Hurtado <fernandohur@gmail.com>
2019-08-26 11:39:45 +0200

    Moved Select to code/pickers

---
+++ code/pickers/Select.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import * as System from "baseui/select";
+import { addPropertyControls, ControlType } from "framer";
+import { controls, merge } from "../generated/Select";
+import { withHOC } from "../withHOC";
+import { useManagedState } from "../utils/useManagedState";
+
+const InnerSelect: React.SFC<any> = ({ options, ...props }) => {
+  const [currentValue, setValue] = useManagedState<Readonly<System.Option[]>>(
+    []
+  );
+
+  return (
+    <System.Select
+      value={currentValue}
+      options={options.map(o => ({ label: o }))}
+      onChange={e => setValue(e.value)}
+      labelKey="label"
+      valueKey="label"
+      searchable={props.type === "search"}
+      {...props}
+    />
+  );
+};
+
+export const Select = withHOC(InnerSelect);
+
+Select.defaultProps = {
+  width: 250,
+  height: 50
+};
+
+addPropertyControls(Select, {
+  options: {
+    type: ControlType.Array,
+    propertyControl: { type: ControlType.String },
+    defaultValue: ["first option", "second option", "third option"]
+  },
+  backspaceRemoves: merge(controls.backspaceRemoves, { defaultValue: true }),
+  closeOnSelect: merge(controls.closeOnSelect, { defaultValue: true }),
+  deleteRemoves: merge(controls.deleteRemoves, { defaultValue: true }),
+  disabled: merge(controls.disabled, {}),
+  error: merge(controls.error, {}),
+  positive: merge(controls.positive, {}),
+  escapeClearsValue: merge(controls.escapeClearsValue, { defaultValue: true }),
+  filterOutSelected: merge(controls.filterOutSelected, {}),
+  isLoading: merge(controls.isLoading, {}),
+  startOpen: merge(controls.startOpen, {}),
+  maxDropdownHeight: {
+    type: ControlType.Number,
+    unit: "px",
+    min: 0,
+    max: 500,
+    defaultValue: 200
+  },
+  noResultsMsg: merge(controls.noResultsMsg, {}),
+  openOnClick: merge(controls.openOnClick, {}),
+  placeholder: merge(controls.placeholder, {}),
+  size: merge(controls.size, {}),
+  type: merge(controls.type, {})
+});
