1c8b2cfe444df60cdd9c1e0d93f1968037894927
Fernando Hurtado <fernandohur@gmail.com>
2019-08-27 12:13:35 +0200

    Cleaned up inputs/Button

--- code/inputs/Button.tsx
+++ code/inputs/Button.tsx
@@ -1,13 +1,16 @@
-import * as React from "react"
 import * as System from "baseui/button"
-import { ControlType, PropertyControls, addPropertyControls } from "framer"
+import { addPropertyControls, ControlType, PropertyControls } from "framer"
+import * as React from "react"
 import { controls, merge } from "../generated/Button"
-import { withHOC } from "../withHOC"
 import { ThemePropertyControl } from "../utils/PropertyControls"
-import { filterProps } from "../utils/FilterProps"
+import { withHOC } from "../withHOC"
 
-const InnerButton: React.SFC<any> = props => {
-  return <System.Button {...filterProps(props, ["willChangeTransform"])}>{props.text}</System.Button>
+const InnerButton: React.SFC<any> = ({ text, ...props }) => {
+  return (
+    <System.Button {...props} $as={props.href ? "a" : "button"}>
+      {text}
+    </System.Button>
+  )
 }
 
 export const Button = withHOC(InnerButton)
@@ -15,28 +18,16 @@ export const Button = withHOC(InnerButton)
 Button.defaultProps = {
   width: 200,
   height: 50,
-  disabled: false,
-  isLoading: false,
-  isSelected: false,
-  kind: System.KIND.primary,
-  overrides: {},
-  shape: System.SHAPE.default,
-  size: System.SIZE.default,
 }
 
 export const ButtonPropertyControls: PropertyControls = {
   href: merge(controls.href, {}),
-  target: merge(controls.target, {}),
-  children: merge(controls.children, {}),
   disabled: merge(controls.disabled, {}),
-  endEnhancer: merge(controls.endEnhancer, {}),
   isLoading: merge(controls.isLoading, {}),
   isSelected: merge(controls.isSelected, {}),
   kind: merge(controls.kind, {}),
   shape: merge(controls.shape, {}),
   size: merge(controls.size, {}),
-  startEnhancer: merge(controls.startEnhancer, {}),
-  type: merge(controls.type, {}),
   text: {
     title: "Text",
     type: ControlType.String,
